61d1f37738ab95a6df52dcd2f98a521645f4bcde
Tony Tsui <tsui.tony@gmail.com>
2017-03-04 00:13:22 +1100

    Show code for all components.

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,34 +1,36 @@
 /* eslint-disable import/no-webpack-loader-syntax */
 import React from "react";
 import Joyride from "react-joyride";
+import AnatomyCard from "./AnatomyCard";
+import TaskInputCode from "!raw-loader!./TaskInput.js";
+import TaskCode from "!raw-loader!./Task.js";
+import TaskFiltersCode from "!raw-loader!./TaskFilters.js";
 import FilterCode from "!raw-loader!./Filter.js";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/styles";
 
 import "react-joyride/lib/react-joyride-compiled.css";
 
 const tourSteps = [{
     title: "Add new task component",
-    text: "This is the TaskInput component",
+    text: <AnatomyCard code={ TaskInputCode }>This is the TaskInput component</AnatomyCard>,
     selector: ".new-todo",
     position: "top",
     type: "click"
   }, {
     title: "Existing task component",
-    text: "This is a Task component",
+    text: <AnatomyCard code={ TaskCode }>This is the Task component</AnatomyCard>,
     selector: ".task-0",
     position: "right",
     type: "click"
 
   }, {
     title: "Filters component",
-    text: "This is a TaskFilters component",
+    text: <AnatomyCard code={ TaskFiltersCode }>This is the TaskFilters component</AnatomyCard>,
     selector: ".filters",
     position: "left",
     type: "click"
   }, {
     title: "A filter component",
-    text: <SyntaxHighlighter language="javascript" style={docco}>{FilterCode}</SyntaxHighlighter>,
+    text: <AnatomyCard code={ FilterCode }>This is the Filter component</AnatomyCard>,
     selector: ".active-filter",
     position: "bottom",
     type: "click"
